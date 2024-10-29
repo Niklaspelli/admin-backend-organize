05e459b2ff2e5532e9b4934d5499d02bce896bfc
Niklas Pelli <niklaspelli22@gmail.com>
2024-10-29 11:11:40 +0100

    dockerfile added

---
+++ ws/simons repos/Frontend-Iorganize/src/pages/login/LoginPage.jsx
@@ -0,0 +1,76 @@
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import styles from "./login.module.css";
+
+const LoginPage = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [message, setMessage] = useState("");
+  const navigate = useNavigate();
+  /* localStorage.setItem('token', token);  */
+
+  const handleLogin = async (e) => {
+    e.preventDefault();
+
+    const requestBody = {
+      email,
+      password,
+    };
+
+    try {
+      const response = await fetch("http://localhost:3000/auth/login", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(requestBody),
+      });
+
+      const data = await response.json();
+
+      if (response.ok) {
+        setMessage("Login successful!");
+        localStorage.setItem("token", data.token);
+        navigate("/workspace");
+      } else {
+        setMessage(data.error || "Login failed");
+      }
+    } catch (error) {
+      setMessage("An error occurred during login");
+    }
+  };
+
+  return (
+    <div className={styles.loginpage}>
+      <h2>Login</h2>
+      <div className={styles.logincomponent}>
+        <form className={styles.form} onSubmit={handleLogin}>
+          <div>
+            <label>Email</label>
+            <input
+              type="email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              required
+            />
+          </div>
+          <div>
+            <label>Password</label>
+            <input
+              type="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              required
+            />
+          </div>
+          <button className={styles.button} type="submit">
+            Login
+          </button>
+        </form>
+        {message && <p>{message}</p>}
+      </div>
+    </div>
+  );
+};
+
+export default LoginPage;
